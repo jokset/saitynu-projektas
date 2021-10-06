13ca18662cd8a0523587b2c9fcb7c4e2e02e71a2
Jokubas Setkauskas <graphframejokubas@gmail.com>
2021-10-06 23:02:07 +0300

    Updated .gitignore

---
+++ api/build/static/js/main.27476276.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpsaitynu-projektas"]=this["webpackJsonpsaitynu-projektas"]||[]).push([[0],{9:function(t,e,s){"use strict";s.r(e);s(0);var n=s(3),r=s.n(n),c=s(1),o=function(){return Object(c.jsx)("h1",{children:"Events system"})};r.a.render(Object(c.jsx)(o,{}),document.querySelector("#root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.27476276.chunk.js.map
