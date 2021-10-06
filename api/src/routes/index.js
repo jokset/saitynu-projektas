78925b0e31ae70358728f629ab308194f90435a0
Jokubas Setkauskas <graphframejokubas@gmail.com>
2021-10-06 22:27:07 +0300

    Fixed index router

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,8 +2,7 @@ const express = require('express')
 const router = new express.Router()
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../build')));
-    app.get('*', (req, res) => {
+    router.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../build', 'index.html'));
     });
 }
